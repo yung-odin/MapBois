aee689db280850f39fbfe6eaa75f8122a2b14893
yung_odin <russgar3@hotmail.com>
2019-11-14 15:24:49 -0600

    Added id var to User class Added new method to set id to 1

--- src/model/User.java
+++ src/model/User.java
@@ -2,11 +2,11 @@ package model;
 
 public class User {
     private String name;
+    private int id;
 
-    public String getName() {
-        return name;
+    public void setIdTo1() {
+        this.id = 1;
     }
-
     public void setName(String name) {
         this.name = name;
     }
