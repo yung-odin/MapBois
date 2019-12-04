ffeaabed20fbe16995e073f3c8ab430c0c6f2628
yung_odin <russgar3@hotmail.com>
2019-12-03 20:07:33 -0600

    Complete overwrite of program to Develop. Please re-pull the entire code and work from there.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
+	public static Stage stage;
+	public AnchorPane layout;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		Main.stage = primaryStage;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("/view/HomePageFXML.fxml"));
+			Scene scene = new Scene(root,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 	
 	public static void main(String[] args) {
---
+++ src/controller/SUFloor2Controller.java
@@ -0,0 +1,6 @@
+package controller;
+
+public class SUFloor2Controller
+{
+
+}
